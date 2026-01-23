12d08fec802fa1ec66813202b35d83d27d8baba3
Kovall-Nick <dworak.niko@gmail.com>
2026-01-23 20:35:20 +0200

    Refactor code: constants and clean up

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int BALL_COUNT = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[3];
+        Ball[] balls = new Ball[BALL_COUNT];
 
         for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        Ball ball = new Ball(colorSupplier.getRandomColor(), random.nextInt(101));
-        return ball;
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_BALL_NUMBER + 1));
     }
 }
